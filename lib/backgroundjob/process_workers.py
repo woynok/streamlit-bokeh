8b9c90e8921b3c2e2a500d957c926f1f6387401a
woynok <woynok@gmail.com>
2023-10-12 11:30:04 +0900

    implementing background job

---
+++ lib/backgroundjob/process_workers.py
@@ -0,0 +1,190 @@
+from __future__ import annotations
+from typing import Literal
+import os
+import yaml
+import traceback
+from dataclasses import dataclass
+from datetime import datetime
+from pytz import timezone
+from ctypes import c_wchar_p
+import multiprocessing
+
+from lib.backgroundjob.test_routine import test_routine
+
+tz = timezone('Asia/Tokyo')
+
+@dataclass
+class WorkerInfo:
+    name: str
+    _last_info_updated_at: str = ""
+    last_status: Literal["not initiated", "ready", "running", "done", "error at app", "error at worker"] = "not initiated"
+    created_at: str = ""
+    updated_at: str = ""
+    message: str = ""
+    tracebacks: list = None
+
+    @property
+    def last_info_updated_at(self)->str:
+        return self._last_info_updated_at
+
+    def to_dict(self)->dict:
+        return {
+            "name": self.name,
+            "last_status": self.last_status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "message": self.message,
+            "tracebacks": [t for t in self.tracebacks if t is not None]
+        }
+    
+    @classmethod
+    def _from_dict(cls, d)->WorkerInfo:
+        return cls(**d)
+
+    def save(self):
+        filepath = f"background_job/info_{self.name}.yaml"
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, "w") as f:
+            yaml.dump(self.to_dict(), f)
+    
+    @classmethod
+    def _from_file(cls, name)->WorkerInfo:
+        import yaml
+        filepath = f"background_job/info_{name}.yaml"
+        with open(filepath, "r") as f:
+            return cls._from_dict(yaml.load(f, Loader=yaml.FullLoader))
+
+    def is_old_info(self, ttl = 0.5)->bool:
+        try:
+            last_info_updated_at = datetime.strptime(self._last_info_updated_at, "%Y-%m-%d %H:%M:%S")
+            return (datetime.now(tz) - last_info_updated_at).total_seconds() > ttl
+        except TypeError:
+            return True
+        except ValueError:
+            return True
+    
+    def is_running_too_long(self, ttl = 300)->bool:
+        updated_at = datetime.strptime(self.updated_at, "%Y-%m-%d %H:%M:%S")
+        # offset aware updated_at by timezone
+        updated_at = updated_at.astimezone(tz)
+        return (datetime.now(tz) - updated_at).total_seconds() > ttl
+        
+    @classmethod
+    def get_from_file(cls, name)->WorkerInfo:
+        try:
+            out = cls._from_file(name)
+        except FileNotFoundError:
+            created_at = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+            out = cls(name = name, created_at = created_at)
+        out._last_info_updated_at = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return out
+    
+    @classmethod
+    def get_from_worker(cls, worker: Worker, save = True)->WorkerInfo:
+        out = cls(name = worker.name).update_values_by_worker(worker)
+        if save:
+            out.save()
+        return out
+
+    def update_values_by_worker(self, worker: Worker, save = True)->WorkerInfo:
+        self._last_info_updated_at = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        self.last_status = worker._status.value
+        self.updated_at = worker._updated_at.value
+        self.message = worker._message.value
+        self.tracebacks = worker._tracebacks[:]
+        print("here you are")
+        if save:
+            print("here you are::saved")
+            self.save()
+        return self
+    
+    @property
+    def status(self)->str:
+        if self.is_running_too_long():
+            return "error at worker"
+        else:
+            return self.last_status
+    
+    __repr__ = __str__ = lambda self: f"<WorkerInfo name={self.name} status={self.status} created_at={self.created_at} updated_at={self.updated_at} message={self.message} tracebacks={self.tracebacks}>"
+    
+class Worker(multiprocessing.Process):
+    def __init__(self, name, routine, routine_args = None, routine_kwargs = None, **kwargs):
+        super().__init__(**kwargs)
+        self.name = name
+        self.routine = routine
+        if routine_args is None:
+            routine_args = []
+        if routine_kwargs is None:
+            routine_kwargs = {}
+        self.routine_args = routine_args
+        self.routine_kwargs = routine_kwargs
+        time_str = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        self.should_stop = multiprocessing.Event()
+        self._status = multiprocessing.Value(c_wchar_p, "ready")
+        self._updated_at = multiprocessing.Value(c_wchar_p, time_str)
+        self._message = multiprocessing.Value(c_wchar_p, "")
+        self._tracebacks = multiprocessing.Array(c_wchar_p, 100)
+        self._info = WorkerInfo.get_from_worker(self)
+    
+    @property
+    def info(self)->WorkerInfo:
+        return WorkerInfo.get_from_file(self.name)
+
+    def update_info_values(self, last_status = "", message = "", tracebacks = None):
+        if tracebacks is None:
+            tracebacks = []
+        updated_at = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        self._status = multiprocessing.Value(c_wchar_p, last_status)
+        self._message = multiprocessing.Value(c_wchar_p, message)
+        self._tracebacks = multiprocessing.Array(c_wchar_p, tracebacks)
+        self._updated_at = multiprocessing.Value(c_wchar_p, updated_at)
+        self._info.update_values_by_worker(self)
+
+    def run(self):
+        self.update_info_values(
+            last_status = "running"
+        )
+        print("print running from worker")
+        print(self._updated_at.value)
+        try:
+            self.routine(*self.routine_args, **self.routine_kwargs)
+            self.update_info_values(
+                last_status = "done"
+            )
+            print("print done from worker")
+            print(self._updated_at.value)
+        except Exception as e:
+            self.update_info_values(
+                last_status = "error at app",
+                message = str(e),
+                tracebacks = traceback.format_exc().splitlines()
+            )
+            print(self._updated_at.value)
+            print(e)
+            print(traceback.format_exc())
+            print("print error from worker")
+
+class ProcessWorkerStoreSingleton:
+    d_workers = {}
+
+    @classmethod
+    def get(cls, key):
+        return cls.d_workers.get(key)
+    
+    @classmethod
+    def set(cls, worker):
+        cls.d_workers[worker.name] = worker
+
+    def is_running(self, key):
+        worker = self.get(key)
+        if worker is not None:
+            return worker.is_alive()
+        else:
+            return False
+    
+    def n_running(self):
+        return sum([self.is_running(key) for key in self.d_workers.keys()])
+
+    def ok_to_start(self, max_n_workers = 2):
+        return self.n_running() < max_n_workers
+    
--- pages/00_test_job_03_mp.py
+++ pages/00_test_job_03_mp.py
@@ -2,19 +2,20 @@ import time
 import streamlit as st
 
 def main():
-    from lib.backgroundjob.process_worker_store import Worker, ProcessWorkerStoreSingleton
+    from lib.backgroundjob.process_workers import Worker, ProcessWorkerStoreSingleton, WorkerInfo
     from lib.backgroundjob.test_routine import test_routine
     st.write(ProcessWorkerStoreSingleton.d_workers)
-    worker: Worker = ProcessWorkerStoreSingleton.get("my-name")
+    worker_unique_name = "my-name"
+    worker: Worker = ProcessWorkerStoreSingleton.get(worker_unique_name)
     if worker:
-        st.write(worker.status.value, worker.is_alive())
+        st.write(worker.info, worker.is_alive())
 
     with st.sidebar:
         is_running = worker is not None and worker.is_alive()
         is_ready = not is_running
         if st.button('Start worker', disabled=is_running):
-            worker = Worker(routine = test_routine, daemon=True)
-            ProcessWorkerStoreSingleton.set("my-name", worker)
+            worker = Worker(name = worker_unique_name, routine = test_routine, daemon=True)
+            ProcessWorkerStoreSingleton.set(worker)
             worker.start()
             st.experimental_rerun()
             
@@ -22,22 +23,25 @@ def main():
             worker.should_stop.set()
             worker.join()
             # worker = None
-            ProcessWorkerStoreSingleton.set("my-name", worker)
+            ProcessWorkerStoreSingleton.set(worker)
             st.experimental_rerun()
 
-    if worker is None:
-        st.markdown('No worker running.')
-    else:
-        st.markdown(f'worker: {worker.pid}')
-        placeholder = st.empty()
-        while worker.is_alive():
-            placeholder_container = placeholder.container()
-            placeholder_container.markdown(f'status: {worker.status.value}')
-            placeholder_container.markdown(f'updated_at: {worker.updated_at.value}')
-            placeholder_container.markdown(f'message: {worker.message.value}')
-            placeholder_container.markdown(f'tracebacks: {worker.tracebacks[:]}')
-            time.sleep(1)
-    st.info("worker is dead")
+    # if worker is None:
+    #     st.markdown('No worker running.')
+    # else:
+    #     st.markdown(f'worker: {worker.pid}')
+    #     placeholder = st.empty()
+    #     while worker.is_alive():
+    #         placeholder_container = placeholder.container()
+    #         placeholder_container.markdown(f'status: {worker.info}')
+    #         time.sleep(1)
+    # st.info("worker is dead")
+    # if worker:
+    #     st.markdown(f'status: {worker.info}')
+    #     time.sleep(2)
+    #     st.markdown(f'status: {worker.info}')
+
+
     # if worker:
     #     st.markdown(f'status: {worker.status.value}')
     #     st.markdown(f'updated_at: {worker.updated_at.value}')
